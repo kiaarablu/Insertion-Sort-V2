e20e8d057e1693749a5da6e893dbb54f52f35db5
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-08-23 10:32:45 +0430

    Add files via upload

---
+++ Inserion_Sort-V2/Selection-Sort-V2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_Sort_V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 15, 22, 8, 12, 53, 84, 73, 81 };
+            Sorting a = new Sorting();
+            a.Sort(array);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Inserion_Sort-V2/Selection-Sort-V2/Sorting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_Sort_V2
+{
+    internal class Sorting
+    {
+        public void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int minindex = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minindex])
+                    {
+                        minindex = j;
+                    }
+
+                }
+                int temp = array[minindex];
+                array[minindex] = array[i];
+                array[i] = temp;
+            }
+            foreach (int item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
